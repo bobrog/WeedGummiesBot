36566fcafa6d5ea78a1dccb1d2aa72dc99149bf0
Tim Robinson <timrobin@gmail.com>
2020-09-24 23:31:18 -0400

    First crack at Google Sheets integration
    Support for file based config of args

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@
 import discord
 import random
 import configargparse
+import gspread
 
 client = discord.Client()
 
@@ -19,20 +20,38 @@ async def on_message(message):
         await message.channel.send('WEED!')
 
     if message.content.startswith('$newtheme'):
-        theme = random.choice(list_of_themes)
+        theme = random.choice(list_of_themes())
         await message.channel.send(theme)
+
+def gsheet_themes():
+    themes = gsheet.col_values(opts.gsheet_col)[opts.gsheet_col_offset:]
+    return list(filter(None, themes))
 
 if __name__ == "__main__":
     # setup/process args
     parser = configargparse.ArgParser()
-    parser.add("-t", "--bot-token", env_var="BOT_TOKEN", required=True, help="Discord bot API token")
-    parser.add("-f", "--theme-file", env_var="THEME_FILE", default="themes.txt", help="File containing themes one per line")
+    parser.add("--config-file", is_config_file=True, help='config file path')
+    parser.add("--discord-token", env_var="DISCORD_TOKEN", required=True, help="Discord bot API token")
+    parser.add("--theme-file", env_var="THEME_FILE", default="themes.txt", help="File containing themes one per line")
+    parser.add("--gsheet-creds-file", env_var="GSHEET_CREDS_FILE", help="Google API Service account key JSON file")
+    parser.add("--gsheet-key", env_var="GSHEET_KEY")
+    parser.add("--gsheet-worksheet", env_var="GSHEET_WORKSHEET")
+    parser.add("--gsheet-col", env_var="GSHEET_COL", type=int)
+    parser.add("--gsheet-col-offset", env_var="GSHEET_COL_OFFSET", type=int)
+
     opts = parser.parse_args()
-    #parser.print_values()
+    parser.print_values()
     
-    # get the themes
-    with open(opts.theme_file, "r") as f:
-        list_of_themes = f.read().splitlines()
-   
+    if opts.gsheet_creds_file != None:
+        # get themes from Google Sheet column
+        gauth = gspread.service_account(filename=opts.gsheet_creds_file)
+        gsheet = gauth.open_by_key(opts.gsheet_key).worksheet(opts.gsheet_worksheet)
+        list_of_themes = gsheet_themes
+    else:
+        # get the themes from file
+        with open(opts.theme_file, "r") as f:
+             file_themes = f.read().splitlines()
+             list_of_themes = lambda: file_themes
+
     # do it live!
-    client.run(opts.bot_token)
+    client.run(opts.discord_token)
