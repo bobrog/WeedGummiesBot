f431811f61052f6e11aa2e2e0ddc0c6e5c77b0c9
Tim Robinson <timrobin@gmail.com>
2022-01-15 16:46:14 -0500

    Lock and make public previous lists
    Formatting
    
    Resolves #19

--- bot.py
+++ bot.py
@@ -15,15 +15,18 @@ from spotipy.oauth2 import SpotifyOAuth
 
 bot = commands.Bot("+", help_command=None)
 
+
 def get_music_channel():
     return discord.utils.get(
-                bot.get_all_channels(),
-                name=opts.reminder_chan_name,
-                type__name=discord.ChannelType.text.name)
+        bot.get_all_channels(),
+        name=opts.reminder_chan_name,
+        type__name=discord.ChannelType.text.name)
+
 
 def gsheet_themes():
     themes = worksheet.col_values(opts.gsheet_col)[opts.gsheet_col_offset:]
     return list(filter(None, themes))
+
 
 def get_stonerism():
     # stoner speak
@@ -35,6 +38,7 @@ def get_stonerism():
     logging.info("stoner: {}".format(say))
     return(say)
 
+
 def get_theme():
     try:
         theme = random.choice(list_of_themes())
@@ -43,13 +47,15 @@ def get_theme():
 
     return theme
 
+
 def delete_theme(theme):
     if opts.gsheet_creds == None:
         file_themes.remove(theme)
     else:
-        cell = worksheet.find(theme,in_column=opts.gsheet_col)
+        cell = worksheet.find(theme, in_column=opts.gsheet_col)
         worksheet.delete_rows(cell.row)
     logging.info("delete_theme: {}".format(theme))
+
 
 def create_playlist(theme, fmt="Tird Tunes - {}"):
     return sp.user_playlist_create(
@@ -58,24 +64,44 @@ def create_playlist(theme, fmt="Tird Tunes - {}"):
         public=False,
         collaborative=True)
 
+
 @bot.command()
+async def util_lock_old_lists(ctx):
+    if (opts.spotify_allowed_users != None and
+        ctx != None and
+        ctx.message.author.name not in opts.spotify_allowed_users.split()
+        ):
+        say = "Restricted"
+        logging.error("util_lock_old_lists: {}".format(say))
+        await ctx.send(say)
+        return
+    plist_ids = list(filter(None, worksheet_plist.col_values(4)[1:]))
+    for p in plist_ids[:-1]:
+        logging.info("Locking and sharing previous list {}".format(
+            sp.playlist(p, fields=["name"])))
+        sp.user_playlist_change_details(
+            sp.me()["id"], p, public=True, collaborative=False)
+
+
+@ bot.command()
 async def help(ctx):
     logging.info("help msg")
     helpemb = discord.Embed(
-            description="** WeedGummies is a kinda buggy, kinda high bot **"
-                + "\n <@{}> for deep thoughts".format(bot.user.id)
-                + "\n\n ** Music Stuff **"
-                + "\n New playlist drops Sunday afternoon in \
+        description="** WeedGummies is a kinda buggy, kinda high bot **"
+        + "\n <@{}> for deep thoughts".format(bot.user.id)
+        + "\n\n ** Music Stuff **"
+        + "\n New playlist drops Sunday afternoon in \
                     <#{}>".format(getattr(get_music_channel(), "id",
-                        opts.reminder_chan_name))
-                + "\n `+gettheme` print a random theme from the spreadshite"
-                + "\n `+playlist NAME` create a playlist"
-                + "\n `+drawplaylist` draws a random theme and creates playlist",
-                colour=0x67eb34,
-            )
+                                          opts.reminder_chan_name))
+        + "\n `+gettheme` print a random theme from the spreadshite"
+        + "\n `+playlist NAME` create a playlist"
+        + "\n `+drawplaylist` draws a random theme and creates playlist",
+        colour=0x67eb34,
+    )
     await ctx.send(embed=helpemb)
 
-@tasks.loop(seconds=3600)
+
+@ tasks.loop(seconds=3600)
 async def newtheme_task():
     logging.info("newtheme_task: woke up")
     channel = get_music_channel()
@@ -94,7 +120,7 @@ async def newtheme_task():
     day = now.strftime("%A")
     hour = now.hour
     minute = now.minute
-    #check if in fire window
+    # check if in fire window
     if day == "Sunday" and hour == 13:
         say = random.choice(reminder_sayings)
         logging.info("newtheme_task: {}".format(say))
@@ -104,24 +130,28 @@ async def newtheme_task():
     else:
         logging.info("newtheme_task: nothing to do")
 
-@bot.event
+
+@ bot.event
 async def on_ready():
     logging.info("We have logged in as {0.user}".format(bot))
     newtheme_task.start()
 
-@bot.command()
+
+@ bot.command()
 async def weed(ctx):
     await ctx.send(get_stonerism())
 
-@bot.listen("on_message")
+
+@ bot.listen("on_message")
 async def weed_listen(message):
     if message.author == bot.user:
         return
 
-    if bot.user.id in [ m.id for m in message.mentions ]:
+    if bot.user.id in [m.id for m in message.mentions]:
         await message.channel.send(get_stonerism())
 
-@bot.command()
+
+@ bot.command()
 async def gettheme(ctx):
     theme = get_theme()
     if theme == None:
@@ -132,14 +162,15 @@ async def gettheme(ctx):
         logging.info("gettheme: {}".format(say))
     await ctx.send(say)
 
-@bot.command()
+
+@ bot.command()
 async def playlist(ctx, *args):
     logging.info("playlist: invoked by '{}' with args '{}'".format(
         ctx.message.author.name,
         " ".join(args)))
 
-    if (opts.spotify_allowed_users != None and 
-        ctx.message.author.name not in opts.spotify_allowed_users.split()):
+    if (opts.spotify_allowed_users != None and
+            ctx.message.author.name not in opts.spotify_allowed_users.split()):
         say = "Playlist creation is restricted"
         logging.error("playlist: {}".format(say))
         await ctx.send(say)
@@ -152,13 +183,15 @@ async def playlist(ctx, *args):
         return
 
     playlist = create_playlist(" ".join(args))
-    logging.info("playlist: created playlist '{}'".format(playlist.get("name")))
+    logging.info("playlist: created playlist '{}'".format(
+        playlist.get("name")))
     mes = await ctx.send("Created playlist {} {}".format(
         playlist.get("name"),
         playlist.get("external_urls").get("spotify")))
     await mes.pin()
 
-@bot.command()
+
+@ bot.command()
 async def drawplaylist(ctx):
     channel = get_music_channel()
     theme = get_theme()
@@ -166,6 +199,14 @@ async def drawplaylist(ctx):
         say = "Out of themes!"
         logging.error("drawplaylist: {}".format(say))
         await channel.send(say)
+        return
+    if (opts.spotify_allowed_users != None and
+        ctx != None and
+        ctx.message.author.name not in opts.spotify_allowed_users.split()
+        ):
+        say = "Playlist creation is restricted"
+        logging.error("playlist: {}".format(say))
+        await ctx.send(say)
         return
     playlist = create_playlist(theme)
     say = "The new playlist theme is ... {} {}".format(
@@ -184,19 +225,20 @@ async def drawplaylist(ctx):
             playlist.get("id")
         ])
         logging.info("drawplaylist: added to playlist history")
+        await util_lock_old_lists(None)
 
 if __name__ == "__main__":
     # setup/process args
     parser = configargparse.ArgParser()
     parser.add("--config-file", is_config_file=True, help="config file path")
     parser.add("--discord-token", env_var="DISCORD_TOKEN", required=True,
-                help="Discord bot API token")
+               help="Discord bot API token")
     parser.add("--theme-file", env_var="THEME_FILE", default="themes.txt",
-                help="File containing themes one per line")
+               help="File containing themes one per line")
     parser.add("--stoner-file", env_var="STONER_FILE", default="stonertalk.txt",
-                help="File containing stoner sayings")
+               help="File containing stoner sayings")
     parser.add("--gsheet-creds", env_var="GSHEET_CREDS",
-                help="Google API Service account key JSON string")
+               help="Google API Service account key JSON string")
     parser.add("--gsheet-key", env_var="GSHEET_KEY")
     parser.add("--gsheet-worksheet", env_var="GSHEET_WORKSHEET")
     parser.add("--gsheet-plist-worksheet", env_var="GSHEET_PLIST_WORKSHEET")
@@ -204,22 +246,22 @@ if __name__ == "__main__":
     parser.add("--gsheet-col-offset", env_var="GSHEET_COL_OFFSET", type=int)
     parser.add("--log-level", env_var="LOG_LEVEL", default="INFO")
     parser.add("--reminder-tz", env_var="REMINDER_TZ",
-                default="America/New_York")
+               default="America/New_York")
     parser.add("--reminder-chan-name", env_var="REMINDER_CHAN_NAME",
-                default="music-andotherstuffrelated")
+               default="music-andotherstuffrelated")
     parser.add("--reminder-file", env_var="REMINDER_FILE",
-                default="reminder.txt")
+               default="reminder.txt")
     parser.add("--spotify-client-id", env_var="SPOTIFY_CLIENT_ID")
     parser.add("--spotify-client-secret", env_var="SPOTIFY_CLIENT_SECRET")
     parser.add("--spotify-client-token", env_var="SPOTIFY_CLIENT_TOKEN")
     parser.add("--spotify-cache-path", env_var="SPOTIFY_CACHE_PATH",
-                default=".env/spotify")
+               default=".env/spotify")
     parser.add("--spotify-scope", env_var="SPOTIFY_SCOPE",
-                default=" ".join([
-                    "playlist-modify-private",
-                    "playlist-read-collaborative",
-                    "playlist-modify-public"
-                ]))
+               default=" ".join([
+                   "playlist-modify-private",
+                   "playlist-read-collaborative",
+                   "playlist-modify-public"
+               ]))
     parser.add("--spotify-allowed-users", env_var="SPOTIFY_ALLOWED_USERS")
 
     opts = parser.parse_args()
@@ -244,8 +286,8 @@ if __name__ == "__main__":
     else:
         # get the themes from file
         with open(opts.theme_file, "r") as f:
-             file_themes = f.read().splitlines()
-             list_of_themes = lambda: file_themes
+            file_themes = f.read().splitlines()
+            def list_of_themes(): return file_themes
         logging.info("themes from file {}".format(opts.theme_file))
 
     # init stoner speak
@@ -264,7 +306,7 @@ if __name__ == "__main__":
             reminder_sayings = f.read().splitlines()
         logging.info("reminders speak from file {}".format(opts.reminder_file))
     else:
-        reminder_sayings = ["New playlist theme drops soon! " \
+        reminder_sayings = ["New playlist theme drops soon! "
                             "Get your suggestions into the spreadshite"]
 
     # init spotify
@@ -280,7 +322,8 @@ if __name__ == "__main__":
         scope=opts.spotify_scope,
         cache_path=opts.spotify_cache_path))
 
-    logging.info("spotify username {}".format(sp.current_user().get("display_name")))
+    logging.info("spotify username {}".format(
+        sp.current_user().get("display_name")))
 
     # do it live!
     bot.run(opts.discord_token)
