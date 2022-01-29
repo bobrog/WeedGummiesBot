0190898e9185e485adab84f7495645bcd359adb3
Tim Robinson <timrobin@gmail.com>
2022-01-29 15:55:01 -0500

    Add error logging  and version

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
-FROM python:3.9.2
+FROM python:3.10.2
 WORKDIR /usr/src/app
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 COPY *.txt ./
 COPY *.py ./
+ENV VERSION=latest
 ENTRYPOINT ["python", "-u", "./bot.py"]
--- deploy.sh
+++ deploy.sh
@@ -12,16 +12,18 @@ git_hash=`git rev-parse HEAD`
 container_version=${image_name}:${git_branch}-${git_hash:0:5}
 docker build -t ${container_version} .
 
+# stop old container
 running_container=`docker ps --filter="name=${image_name}_${git_branch}" -q`
 if [ -n "${running_container}" ]; then
     docker stop ${running_container}
     docker rm ${running_container}
 fi
 
-# run in headless/always on mode
+# start new container in headless/always on mode
 docker run \
     -d \
     --restart always \
     --env-file ${env_file} \
+    --env VERSION=${container_version} \
     --name ${image_name}_${git_branch} \
     ${container_version}
